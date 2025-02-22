8489f5a25c5a8ab4fc873c4362e8ff22819569ca
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-02-22 00:00:02 -0300

    Configurando autenticacao do user e relacionamento de medicoes

--- app/models/medicao.rb
+++ app/models/medicao.rb
@@ -1,3 +1,7 @@
 class Medicao < ApplicationRecord
   belongs_to :user
+  validates :data, presence: true
+  validates :medicao_manha, :medicao_tarde, numericality: { greater_than_or_equal_to: 0 }
+  
+  
 end
