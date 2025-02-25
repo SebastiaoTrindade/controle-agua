a8847f0ea936a72022a6448c94eb6c445359686e
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-02-25 14:52:33 -0300

    Implantando a autenticação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :logged_in_user
+  
+  private
+
+  def logged_in_user
+    unless logged_in?     
+    flash[:danger] = "Por favor, faca login."
+    redirect_to login_url unless request.path == login_url
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # Session Login
+  root 'sessions#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to:'sessions#destroy'
+  
   resources :medicaos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
